77ed057e70edb15b0d8f2de163f13c37137a435c
Casey Watson <cwatson@drndata.com>
2014-07-16 15:03:38 -0500

    Initial commit.

---
+++ TableStorageCopier/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorageCopier
+{
+    internal class Program
+    {
+        private const string DestinationStorageConnectionString = "";
+        private const string DestinationTableName = "";
+        private const string SourceStorageConnectionString = "";
+        private const string SourceTableName = "";
+
+        private static void Main(string[] args)
+        {
+            var sourceCloudStorageAccount = CloudStorageAccount.Parse(SourceStorageConnectionString);
+            var destinationCloudStorageAccount = CloudStorageAccount.Parse(DestinationStorageConnectionString);
+
+            var sourceTableStorageClient = sourceCloudStorageAccount.CreateCloudTableClient();
+            var destinationTableStorageClient = destinationCloudStorageAccount.CreateCloudTableClient();
+
+            var sourceTable = sourceTableStorageClient.GetTableReference(SourceTableName);
+            var destinationTable = destinationTableStorageClient.GetTableReference(DestinationTableName);
+
+            destinationTable.CreateIfNotExists();
+
+            var tableQuery = new TableQuery<RawTableEntity>();
+
+            foreach (var entity in sourceTable.ExecuteQuery(tableQuery))
+            {
+                var insertOp = TableOperation.Insert(entity);
+                destinationTable.Execute(insertOp);
+            }
+        }
+    }
+}
---
+++ TableStorageCopier/RawTableEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace TableStorageCopier
+{
+    public class RawTableEntity : ITableEntity
+    {
+        public string ETag { get; set; }
+        public string PartitionKey { get; set; }
+        public IDictionary<string, EntityProperty> Properties { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset Timestamp { get; set; }
+
+        public void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+        {
+            Properties = properties;
+        }
+
+        public IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+        {
+            return Properties;
+        }
+    }
+}
